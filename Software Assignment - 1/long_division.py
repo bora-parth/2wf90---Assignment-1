8baae57ff7352a0a8ab56073f13ef90d18010e08
denewulf <boraparth02@gmail.com>
2021-09-24 17:55:00 +0200

    almost done with all the methods

--- Software Assignment - 1/long_division.py
+++ Software Assignment - 1/long_division.py
@@ -1,6 +1,48 @@
 #helper functions
-from main import subtraction, addition, multiplicationPrimary
+from main import subtraction, addition, multiplicationPrimary, trimZeroes, multiplicationKaratsuba
 
+def add(x, y, radix):
+    if (x[0] != '-') & (y[0] != '-'):
+        result = addition(x, y, radix, 0, 0)[0]
+        
+    if (x[0] != '-') & (y[0] == '-'):
+        result = subtraction(x, y[1:], radix, 0, 0)[0]
+        
+    if (x[0] == '-') & (y[0] != '-'):
+        result = subtraction(y, x[1:], radix, 0, 0)[0]
+
+    if (x[0] == '-') & (y[0] == '-'):
+        result = addition(x[1:], y[1:], radix,  0, 0)[0]
+
+    return result
+
+def subtract(x, y, radix):
+    if (x[0] != '-') & (y[0] != '-'):
+        result = subtraction(x, y, radix, 0, 0)[0]
+        # a-(-b) = a+b
+    if (x[0] != '-') & (y[0] == '-'):
+        result = addition(x, y[1:], radix, 0, 0)[0]
+    if (x[0] == '-') & (y[0] != '-'):
+        result = addition(y, x[1:], radix, 0, 0)[0]
+    if (x[0] == '-') & (y[0] == '-'):
+        result = subtraction(y[1:], x[1:], radix, 0, 0)[0]
+
+    return result
+
+def multiply(x, y, radix):
+    if (x[0] != '-') & (y[0] != '-'):
+        result = multiplicationKaratsuba(x, y, radix, 0, 0)[0]
+    
+    if (x[0] == '-') & (y[0] != '-'):
+        result = multiplicationKaratsuba(x[1:], y, radix, 0, 0)[0]
+        
+    if (x[0] != '-') & (y[0] == '-'):
+        result = multiplicationKaratsuba(x, y[1:], radix, 0, 0)[0]
+       
+    if (x[0] == '-') & (y[0] == '-'):
+        result = multiplicationKaratsuba(y[1:], x[1:], radix, 0, 0)[0]
+           
+    return result
 #converts the input strings to an array
 
 def string_to_list(num):
@@ -45,7 +87,6 @@ def list_to_string(num_list):
 def bigger(x, y, radix):
     if x == y:
         return 0
-
     x = string_to_list(x)
     y = string_to_list(y)
     sign_x = '+'
@@ -56,16 +97,16 @@ def bigger(x, y, radix):
     if y[0] == '-':
         sign_y = '-'
 
-    if sign_x == '+' and sign_y == 'y':
+    if sign_x == '+' and sign_y == '-':
         return list_to_string(x)
 
     if sign_x == '-' and sign_y == '+':
         return list_to_string(y)
 
-    res = subtraction(x, y, radix, 0, 0)
+    res = subtraction(list_to_string(x), list_to_string(y), radix, 0, 0)[0]
 
     sign_res = '+'
-    if subtraction[0] == '-':
+    if res[0] == '-':
         sign_res = '-'
 
     if sign_res == sign_x:
@@ -79,6 +120,7 @@ def bigger(x, y, radix):
 #
 def long_division(x, y, radix):
 
+    
     #check the sign of the numbers
     sign_x = '+'
     if x[0] == '-':
@@ -98,7 +140,7 @@ def long_division(x, y, radix):
 
     #if y>x then output 0 as quotient and x as the remainder
     if k < l:
-        return [0, x]
+        return ['0', list_to_string(x)]
 
     #quotient and remainder
     q = [] 
@@ -141,10 +183,10 @@ def long_division(x, y, radix):
 
     q = list_to_string(q)
     r = list_to_string(r)
-
     #remove zeroes at the beginning of the numbers
-    q = q.lstrip("0")
-    r = r.lstrip("0")
+
+    q = trimZeroes(q)
+    r = trimZeroes(r)
 
     return [q,r]
 
@@ -160,11 +202,12 @@ def modular_addition(x, y, m, radix):
     a = modular_reduction(x, m, radix)
     b = modular_reduction(y, m, radix)
     #z1 = z'
-    z1 = addition(a,b, radix,0,0)[0]
+    z1 = add(a, b, radix)
+    
     if bigger(z1, m, radix) == m:
         z = z1
     else:
-        z = subtraction(z1,m, radix)
+        z = subtract(z1, m, radix)
     return z
 
 #modular subtraction function
@@ -173,11 +216,11 @@ def modular_subtraction(x, y, m, radix):
     a = modular_reduction(x, m, radix)
     b = modular_reduction(y, m, radix)
     #z1 = z'
-    z1 = subtraction(a,b, radix,0,0)[0]
+    z1 = subtract(a,b, radix)
     if z1[0] != '-':
         z = z1
     else:
-        z = addition(z1,m, radix)
+        z = add(z1,m, radix)
     return z
 
 #modular muliplication function
@@ -186,28 +229,31 @@ def modular_multiplication(x, y, m, radix):
     a = modular_reduction(x, m, radix)
     b = modular_reduction(y, m, radix)
     #z1 = z'
-    z1 = multiplicationPrimary(a, b, radix)
+    z1 = multiply(a, b, radix)
     z = modular_reduction(z1, m, radix)
     return z
 
-def modular_inversion(x, m, radix):
-    x1 = x #a'
+def modular_inversion(a, m, radix):
+    a1 = modular_reduction(a, m, radix) #a'
     m1 = m
-    a1 = '1' #x1
-    a2 = '2' #x2
-    while m1[0] != '-' or m1[0] != '0':
+    x1 = '1' #x1
+    x2 = '0' #x2
+    while m1[0] != '-' or m1 != '0':
 
-        q = long_division(x1, m1, radix)[0]
-        temp = multiplicationPrimary(q, m1, radix, 0, 0)
-        r = subtraction(x1, temp)
+        q = long_division(a1, m1, radix)[0]
+        temp = multiply(q, m1, radix)
+        r = subtract(a1, temp, radix)
         a1 = m1
         m1 = r
-        temp = multiplicationPrimary(q, a2, radix, 0, 0)
-        a3 = subtraction(a1, temp)
-        a1 = a2
-        a2 = a3
+        temp1 = multiply(q, x2, radix)
+        x3 = subtract(x1, temp1, radix)
+        x1 = x2
+        x2 = x3
 
-    if x1 == '1':
+        if m1 == '0':
+            break
+
+    if a1 == '1':
         return x1
     else:
         return "ERROR - INVERSE DOESN'T EXIST"
@@ -215,4 +261,4 @@ def modular_inversion(x, m, radix):
 
 a = 'd26936c465648ef03a1ade904737b30428155781'
 b = '157f77a46f4c796bb774'
-print(modular_addition(a,b, '12', 16))
+print(modular_inversion('3','11', 10))
